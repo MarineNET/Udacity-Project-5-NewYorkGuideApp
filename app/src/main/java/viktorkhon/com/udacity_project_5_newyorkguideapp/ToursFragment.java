8949edb797ec1ac23c1c9e9cce1a919b5064965c
Viktor Khon <betepokus@yahoo.com>
2017-08-14 00:20:36 -0400

    Finished with Tours Fragments

--- app/src/main/java/viktorkhon/com/udacity_project_5_newyorkguideapp/ToursFragment.java
+++ app/src/main/java/viktorkhon/com/udacity_project_5_newyorkguideapp/ToursFragment.java
@@ -27,8 +27,14 @@ public class ToursFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.list_view, container, false);
 
         ArrayList<Recommendations> recs = new ArrayList<>();
-        recs.add(new Recommendations(R.string.barbetta, R.string.barbetta_descr));
-        recs.add(new Recommendations(R.string.barbetta, R.string.barbetta_descr));
+        recs.add(new Recommendations(R.string.top_of_the_rock, R.string.top_of_the_rock_desc));
+        recs.add(new Recommendations(R.string.empire_state, R.string.empire_state_desc));
+        recs.add(new Recommendations(R.string.statue_liberty, R.string.statue_liberty_desc));
+        recs.add(new Recommendations(R.string.museum_natural_history, R.string.museum_natural_history_desc));
+        recs.add(new Recommendations(R.string.museum_modern_arts, R.string.museum_modern_arts_desc));
+        recs.add(new Recommendations(R.string.big_bus, R.string.big_bus_desc));
+        recs.add(new Recommendations(R.string.tribute_9_11, R.string.tribute_9_11_desc));
+        recs.add(new Recommendations(R.string.the_met, R.string.the_met_desc));
 
         final AttractionAdapter adapter = new AttractionAdapter(getActivity(), recs);
         final ListView listView = (ListView) rootView.findViewById(R.id.listView);
